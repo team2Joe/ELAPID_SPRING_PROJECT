2947a033bff4eb67951cdd555cceba1b6b7701e2
jjhwnn <jeahwan174@gmail.com>
2022-04-19 20:32:11 +0900

    update jjh_controller

--- Elapid_Spring/src/main/java/com/elapid/spring01/controller/EController_JJH.java
+++ Elapid_Spring/src/main/java/com/elapid/spring01/controller/EController_JJH.java
@@ -34,11 +34,11 @@ public class EController_JJH {
 	}
 	
 	//메인 화면
-	@RequestMapping("main")
-	public String main() {
-		
-		return "index";
-	}
+//	@RequestMapping("main")
+//	public String main() {
+//		
+//		return "index";
+//	}
 	
 	//캐리어 전체 리스트 
 	@RequestMapping("luggageList")
